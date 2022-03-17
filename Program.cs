80f92eb089890e2be38d83ccfc568af09f7e1027
andressaanunes <andressa_assuncao@live.com>
2022-03-17 08:48:23 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace roteiro2
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            //Exercicio1
+            int a, b, c;
+
+            a = 5;
+            b = 2;
+            c = a - b;
+            Console.WriteLine("a - b ={0} ", c);
+
+        }
+
+    }
+}
